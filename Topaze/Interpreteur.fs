412d72d4e718ab55f17fc06ce04271783905c52e
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-05-29 10:33:43 -0400

    Ajout interpréteur

---
+++ Topaze/Interpreteur.fs
@@ -0,0 +1,14 @@
+﻿module Interpreteur
+
+let mutable variables = Map.empty
+
+let créer nom valeur = 
+    variables <- variables.Add(nom, valeur)
+
+let evaluerExpression = 
+    let rec evalInterieur expression = 
+        match expression with
+        | Initialisation_Chaine (nom, valeur) -> créer nom valeur
+        | Affichage (message) -> printfn "%s" message
+    evalInterieur
+
--- Topaze/Program.fs
+++ Topaze/Program.fs
@@ -1,21 +1,7 @@
-﻿let mutable variables = Map.empty
-
-let créer nom valeur = 
-    variables <- variables.Add(nom, valeur)
-    printfn "variable %s est maintenant %s" nom valeur
-
-let evaluerExpression = 
-    let rec evalInterieur expression = 
-        match expression with
-        | Initialisation_Chaine (nom, valeur) -> créer nom valeur
-        | Affichage (message) -> printfn "%s" message
-    evalInterieur
-
-let nouvelleInstruction = Initialisation_Chaine("x", "bonjour")
-
-evaluerExpression nouvelleInstruction
+﻿let nouvelleInstruction = Initialisation_Chaine("x", "bonjour")
 
 [<EntryPoint>]
 let main argv = 
+    Interpreteur.evaluerExpression nouvelleInstruction
     Parseur.testage
     0
