b5d6b9446e70e50d141d36c9a58fbe45ab571efd
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-05-29 12:09:47 -0400

    Parser initialisation

--- Topaze/Parseur.fs
+++ Topaze/Parseur.fs
@@ -10,14 +10,23 @@ let test p str =
 let ws = skipManySatisfy (fun c -> c = ' ' || c = '\t' || c='\r')
 
 let estChaine c = isLetter c || isDigit c || c = ' '
+let estNomVariable c = isLetter c || isDigit c
 
 let pChaineEntreGuillemets = pstring "\"" >>. many1Satisfy estChaine .>> pstring "\""
 
 let pCommande s = pstringCI s .>> ws
 
+let pNomVariable = many1Satisfy estNomVariable .>> ws
+
+let pNomVaut = pNomVariable .>> pstringCI "vaut" .>> ws
+
 let pAffichage = pCommande "afficher" >>. pChaineEntreGuillemets |>> (fun chaine -> Affichage(chaine))
 
+let pInitialisation = pNomVaut .>>. pChaineEntreGuillemets |>> (fun (nom, valeur) -> Initialisation_Chaine(nom, valeur))
+
+let pInstructions = pAffichage <|> pInitialisation
+
 let parse (program:string) =    
-    match run pAffichage program with
+    match run pInstructions program with
     | Success(result, _, _)   -> result 
     | Failure(errorMsg, e, s) -> failwith errorMsg
--- Topaze/Program.fs
+++ Topaze/Program.fs
@@ -2,4 +2,7 @@
 let main argv = 
     let resultatParsing = Parseur.parse "AFFICHER \"Bonjour Mondé\""
     Interpreteur.evaluerExpression resultatParsing
+    let resultatParsing = Parseur.parse "Nom vAuT \"Olivier Lafleur\""
+    Interpreteur.evaluerExpression resultatParsing
+    Interpreteur.variables |> Map.iter (fun key value -> printfn "[%s;%s]" key value)
     0
